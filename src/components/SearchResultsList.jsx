26bff9101227517ea6dee308db63f4f6ef365b97
Christian <c.espinoza.code@gmail.com>
2024-06-21 14:46:16 -0500

    searchResultsList component styles from css to tailwind

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -3,7 +3,7 @@ import { SearchResult } from "./SearchResult";
 
 export const SearchResultsList = ({ results }) => {
   return (
-    <div className="results-list">
+    <div className="w-full bg-white flex flex-col shadow-md rounded-lg mt-4 max-h-[300px] overflow-y-auto">
       {results.map((result, id) => {
         return <SearchResult result={result.name} key={id} />;
       })}
